3a6c6167afda4ef895d577d8ebb245a462f998df
qyz17 <qyz17@mails.tsinghua.edu.cn>
2020-03-08 20:54:28 +0800

    add table in result page

--- search/src/components/Result.vue
+++ search/src/components/Result.vue
@@ -4,6 +4,25 @@
     <br><br>
     <p>You have searched for {{query}}</p><br><br><br>
     <button type="button" class="buttons" v-on:click="backHome">Return</button>
+    <table style="width: 100%">
+      <tr>
+        <td style="width: 20%">
+          搜索时间<br>
+          时间1<br>
+          时间2<br>
+        </td>
+        <td style="width: 60%">
+          结果1<br>
+          结果2<br>
+          结果3<br>
+        </td>
+        <td style="width: 20%">
+          something else<br>
+          lalala
+        </td>
+      </tr>
+
+    </table>
   </div>
 </template>
 
