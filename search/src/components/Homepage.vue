1123cea0ca30084ea3f9fbc651b4c309272bb83a
SigmaTsing <xushangqingxsq@126.com>
2020-05-06 17:44:37 +0800

    feat(tuto):add simple tutorial

--- search/src/components/Homepage.vue
+++ search/src/components/Homepage.vue
@@ -5,27 +5,23 @@
 
       <el-input class='search' v-model="query" placeholder="What do you want to know?"></el-input> <br>
       <p>
-        <el-radio style="zoom: 120%" v-model="search_type" label="scholar">Search for scholars</el-radio>
+        <el-radio style="zoom: 120%" v-model="search_type" label="author">Search for scholars</el-radio>
         <el-radio style="zoom: 120%" v-model="search_type" label="paper">Search for papers</el-radio>
       </p>
 
       <el-button type="primary" style="font-size: 15px" icon="el-icon-search" v-on:click="toSearch">Search</el-button>
 
       <br><br>
-      <body style="height: 200px; width: 60%; margin: 0 auto;" bgcolor="white">
-        <span class="demonstration">Tutorial</span>
-        <el-carousel trigger="click" height="150px" style="vertical-align: center">
+      <body style="height: 400px; width: 800px; margin: 0 auto;" bgcolor="white">
+        <el-carousel trigger="click" height="400px" style="vertical-align: center">
           <el-carousel-item style="">
-            1<br>
-            This is a tutorial.
+            <img src="../assets/tutorial1.png" width="100%">
           </el-carousel-item>
           <el-carousel-item>
-            2<br>
-            We show you how our search engine works.
+            <img src="../assets/tutorial2.png" width="100%">
           </el-carousel-item>
           <el-carousel-item>
-            3<br>
-            Here you can find scholars that are most similar to the one you search for.
+            <img src="../assets/tutorial3.png" width="100%">
           </el-carousel-item>
         </el-carousel>
       </body>
@@ -46,7 +42,7 @@
           return {
               query: "",
               results: {},
-              search_type: 'scholar',
+              search_type: 'author',
           }
       },
       methods: {
